7adc3b95b332f772b82160235a514849bba344c2
Antonio Dias <antonio@cubotecnologiabr.com.br>
2025-11-22 13:52:57 -0300

    [feat][main] - Adicionada models de chat

---
+++ app/Domain/Models/Chat.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Domain\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Chat extends Model
+{
+    use HasUuids;
+
+    protected $fillable = [
+        'uuid',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function chatMessages(): HasMany
+    {
+        return $this->hasMany(ChatMessage::class);
+    }
+}
---
+++ app/Domain/Models/ChatMessage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Domain\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ChatMessage extends Model
+{
+    protected $fillable = [
+        'chat_id',
+        'content',
+        'role',
+        'chat_id'
+    ];
+
+    public function chat(): BelongsTo
+    {
+        return $this->belongsTo(Chat::class);
+    }
+}
