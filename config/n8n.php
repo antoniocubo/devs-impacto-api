065ca3d97d8314e227674959b660fb281a9bc303
Antonio Dias <antonio@cubotecnologiabr.com.br>
2025-11-23 15:11:23 -0300

    [feat][main] - Adicionada logica para url de test

--- config/n8n.php
+++ config/n8n.php
@@ -3,5 +3,5 @@
 return [
     "api_key" => env("N8N_API_KEY"),
     "auth_header_name" => env("N8N_AUTH_HEADER_NAME", "Authorization"),
-    "chat_base_url" => env("N8N_CHAT_BASE_URL", "http://localhost:5678"),
+    "chat_base_url" => env("N8N_CHAT_PROD_BASE_URL", "N8N_CHAT_TEST_BASE_URL"),
 ];
